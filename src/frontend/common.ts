eb6c0a88ea327bc25a05a1907f0f4f340fbef098
ozo-vehe <johnnie.vehe@gmail.com>
2024-03-16 22:56:26 +0100

    new changes

--- src/frontend/common.ts
+++
@@ -1,9 +0,0 @@
-export function getImageSource(imageData: Uint8Array | number[]) {
-    if (imageData != null) {
-        const array = Uint8Array.from(imageData);
-        const blob = new Blob([array.buffer], { type: 'image/png' });
-        return URL.createObjectURL(blob);
-    } else {
-        return "";
-    }
-}
