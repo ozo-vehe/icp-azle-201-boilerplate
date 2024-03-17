4157d157600305311b141a5abb2b4fb146793416
John Obansa <johnnie.vehe@gmail.com>
2024-03-17 20:47:36 +0000

    edited the render function in the index.js file

--- src/dfinity_js_frontend/src/index.js
+++ src/dfinity_js_frontend/src/index.js
@@ -4,23 +4,26 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { initializeContract } from "./utils/icp";
 
+console.log("Yes")
+console.log(initializeContract())
+
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// window.renderICPromise = initializeContract()
-//   .then(() => {
-//     ReactDOM.render(
-//       <React.StrictMode>
-//         <App />
-//       </React.StrictMode>,
-//       document.getElementById("root")
-//     );
-//   }).catch(console.error);
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-render(<App />, document.getElementById("root"));
+window.renderICPromise = initializeContract()
+  .then(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById("root")
+    );
+    render(<App />, document.getElementById("root"));
+  }).catch(console.error);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 reportWebVitals();
